1e0ee6f8e9b54ad7f95cc9c0263f26c29db974ab
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-15 15:29:32 +0200

    TeamPage component

---
+++ src/components/TeamPage.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom"
+
+function TeamPage() {
+    const { teamId } = useParams()
+  return (
+    <div>
+      Team page for {teamId}
+    </div>
+  )
+}
+
+export default TeamPage
