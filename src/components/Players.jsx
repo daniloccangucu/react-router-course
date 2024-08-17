eabeee8304e8e7de2033c8845f1dc555f64f23d1
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-17 16:45:19 +0200

    Players and TeamPage components

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,6 +1,12 @@
+import { useSearchParams } from "react-router-dom"
+
 export default function Players() {
+    const [searchParams] = useSearchParams();
+    const team = searchParams.get('teamId')
+
     return (
-        <div className="container">Players
-    </div>
+        <div className="container">
+            Players for team {team}
+        </div>
     )
 }
