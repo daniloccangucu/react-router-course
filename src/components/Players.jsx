0f6b6160fd2aa4c4316bd8925a7aba20c746f3b8
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-18 17:01:48 +0200

    Sidebar and CustomLink component in Players

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,11 +1,65 @@
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams, Link, useMatch } from "react-router-dom"
+import usePlayerNames from "../hooks/usePlayerNames"
+import { slugify } from "../utils";
+
+function CustomLink ({ to, children }) {
+    const match = useMatch(to)
+
+    const styles = match
+        ? { fontWeight: 900, color: '(var--white)'}
+        : {}
+
+    return (
+        <li>
+            <Link
+                style={{...styles}}
+                to={to}
+            >
+                {children}
+            </Link>
+        </li>
+    )
+}
+
+function Sidebar ({ title, list }) {
+    return (
+        <div>
+            <h3 className="header">
+                {title}
+            </h3>
+            <ul className="sidebar-list">
+                {list.map((item) => (
+                    <CustomLink
+                        key={item}
+                        to={slugify(item)}
+                    >
+                        {item.toUpperCase()}
+                    </CustomLink>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default function Players() {
     const [searchParams] = useSearchParams();
     const team = searchParams.get('teamId')
 
+    const {
+        response: names,
+        loading
+    } = usePlayerNames(team)
+
+    if (loading) {
+        return null
+    }
+
     return (
         <div className="container">
+            <Sidebar
+                title="Players"
+                list={names}
+            />
             Players for team {team}
         </div>
     )
