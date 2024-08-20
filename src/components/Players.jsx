10a41889e8200d83a8b4c07e843c1139d3369d1e
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-20 20:33:23 +0200

    Players component refactored

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,45 +1,7 @@
-import { useSearchParams, Link, useMatch } from "react-router-dom"
+import { useSearchParams, Outlet } from "react-router-dom"
+
 import usePlayerNames from "../hooks/usePlayerNames"
-import { slugify } from "../utils";
-
-function CustomLink ({ to, children }) {
-    const match = useMatch(to)
-
-    const styles = match
-        ? { fontWeight: 900, color: '(var--white)'}
-        : {}
-
-    return (
-        <li>
-            <Link
-                style={{...styles}}
-                to={to}
-            >
-                {children}
-            </Link>
-        </li>
-    )
-}
-
-function Sidebar ({ title, list }) {
-    return (
-        <div>
-            <h3 className="header">
-                {title}
-            </h3>
-            <ul className="sidebar-list">
-                {list.map((item) => (
-                    <CustomLink
-                        key={item}
-                        to={slugify(item)}
-                    >
-                        {item.toUpperCase()}
-                    </CustomLink>
-                ))}
-            </ul>
-        </div>
-    )
-}
+import Sidebar from "./Sidebar";
 
 export default function Players() {
     const [searchParams] = useSearchParams();
@@ -55,12 +17,12 @@ export default function Players() {
     }
 
     return (
-        <div className="container">
+        <div className="container two-column">
             <Sidebar
                 title="Players"
                 list={names}
             />
-            Players for team {team}
+            <Outlet />
         </div>
     )
 }
