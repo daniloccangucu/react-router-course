6ebb04a635d17d2f938fa134196fdae176511ee0
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-20 20:32:14 +0200

    nested routes for players, teams and :teamId

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,10 @@ import Home from "./Home"
 import Teams from "./Teams"
 import Players from "./Players"
 import TeamPage from "./TeamPage"
+import Player from "./Player"
+import Team from "./Team"
+import Article from "./Article"
+import Articles from "./Articles"
 
 export default function App () {
   return (
@@ -13,9 +17,16 @@ export default function App () {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/players" element={<Players />} />
-        <Route path="/teams" element={<Teams />} />
+        <Route path="/players" element={<Players />}>
+          <Route path=":playerId" element={<Player />}/>
+        </Route>
+        <Route path="/teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />}/>
+        </Route>
         <Route path="/:teamId" element={<TeamPage />} />
+        <Route path="/:teamId/articles" element={<Articles />}>
+          <Route path=":articleId" element={<Article />}/>
+        </Route>
       </Routes>
     </div>
     </Router>
