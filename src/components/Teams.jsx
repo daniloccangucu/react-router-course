4f6bb41012b13b108336e6fe3cf5874841dfd725
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-20 20:34:38 +0200

    Teams component refactored

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,5 +1,25 @@
+import { Outlet } from "react-router-dom"
+
+import useTeamNames from "../hooks/useTeamNames"
+import Sidebar from "./Sidebar"
+
 export default function Teams() {
+    const {
+        response: teamNames,
+        loading
+    } = useTeamNames();
+
+    if (loading) {
+        return null;
+    }
+
     return (
-        <div className="container">Teams</div>
+        <div className="container two-column">
+            <Sidebar 
+                title="Teams"
+                list={teamNames}
+            />
+            <Outlet />
+        </div>
     )
 }
