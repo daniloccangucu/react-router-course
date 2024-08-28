599143ba1d70bb5d3608557b85477dff44af54a5
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-28 15:20:00 +0200

    Added lazy loading and Suspense for component optimization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import NavBar from "./NavBar"
-import Home from "./Home"
-import Teams from "./Teams"
-import Players from "./Players"
-import TeamPage from "./TeamPage"
-import Player from "./Player"
-import Team from "./Team"
-import Article from "./Article"
-import Articles from "./Articles"
+import NavBar from "./NavBar";
+import Loading from "./Loading";
+
+const Home = lazy(() => import("./Home"))
+const Teams = lazy(() => import("./Teams"))
+const Players = lazy(() => import("./Players"))
+const TeamPage = lazy(() => import("./TeamPage"))
+const Player = lazy(() => import("./Player"))
+const Team = lazy(() => import("./Team"))
+const Article = lazy(() => import("./Article"))
+const Articles = lazy(() => import("./Articles"))
 
 export default function App () {
   return (
     <Router>
     <div>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/players" element={<Players />}>
-          <Route path=":playerId" element={<Player />}/>
-          <Route path="" element={<div className="sidebar-instruction">Select a Player</div>}/>
-        </Route>
-        <Route path="/teams" element={<Teams />}>
-          <Route path=":teamId" element={<Team />}/>
-          <Route path="" element={<div className="sidebar-instruction">Select a Team</div>}/>
-        </Route>
-        <Route path="/:teamId" element={<TeamPage />} />
-        <Route path="/:teamId/articles" element={<Articles />}>
-          <Route path=":articleId" element={<Article />}/>
-          <Route path="" element={<div className="sidebar-instruction">Select an Article</div>}/>
-        </Route>
-      </Routes>
+
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/players" element={<Players />}>
+            <Route path=":playerId" element={<Player />}/>
+            <Route path="" element={<div className="sidebar-instruction">Select a Player</div>}/>
+          </Route>
+          <Route path="/teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />}/>
+            <Route path="" element={<div className="sidebar-instruction">Select a Team</div>}/>
+          </Route>
+          <Route path="/:teamId" element={<TeamPage />} />
+          <Route path="/:teamId/articles" element={<Articles />}>
+            <Route path=":articleId" element={<Article />}/>
+            <Route path="" element={<div className="sidebar-instruction">Select an Article</div>}/>
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
     </Router>
   )
